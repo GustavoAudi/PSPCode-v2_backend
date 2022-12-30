125493e404b01bdf0fd5366f620a5b4b0323ce80
Gustavo <gusta97_2009@hotmail.com>
2022-12-29 21:20:59 -0300

    fix development files access

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
     config.enable_processing = false
     config.asset_host = ENV['SERVER_URL']
   else
-    break if ENV['AWS_ACCESS_KEY_ID'].blank? || ENV['AWS_SECRET_ACCESS_KEY'].blank? ||
-      ENV['S3_BUCKET_NAME'].blank?
+    break if ENV['BUCKET_ACCESS_KEY_ID'].blank? || ENV['BUCKET_SECRET_ACCESS_KEY'].blank? ||
+      ENV['S3_BUCKET_NAME'].blank? || ENV['BUCKET_HOST'].blank? || ENV['BUCKET_ENDPOINT'].blank?
 
     config.storage = :fog
     config.fog_credentials = {
